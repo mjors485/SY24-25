def91b44f3c8be127a7f9ba0d0236a6537b40b25
mjors485 <mjors485@learner.dvusd.org>
2025-02-12 09:09:30 -0700

    2/12 quartet start

---
+++ Quartet/Quartet/CarCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quartet
+{
+    internal class CarCard
+    {
+        public int maxSpeed {  get; set; }
+        public double zeroTo60 { get; set; }
+        public int HP { get; set; }
+        public int CC { get; set; }
+        public int cylinders { get; set; }
+        public int RPM { get; set; }
+
+    }
+}

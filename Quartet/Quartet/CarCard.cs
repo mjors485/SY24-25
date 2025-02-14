51478336a37eae5b82c1b5772fdc4a8caffae0b5
mjors485 <mjors485@learner.dvusd.org>
2025-02-14 09:33:38 -0700

    2/14 quartet update

--- Quartet/Quartet/CarCard.cs
+++ Quartet/Quartet/CarCard.cs
@@ -9,22 +9,81 @@ namespace Quartet
 {
     internal class CarCard
     {
+        public enum category { ID, maxSpeed, zeroTo60, HP, CC, cylinders, RPM }
         public string name {  get; set; }
+        public string ID { get; set; }
         public int maxSpeed {  get; set; }
         public double zeroTo60 { get; set; }
         public int HP { get; set; }
         public int CC { get; set; }
         public int cylinders { get; set; }
         public int RPM { get; set; }
-        public CarCard(string name, int maxSpeed, double zeroTo60, int hP, int cC, int cylinders, int rPM)
+        
+        public CarCard(string name, string ID, int maxSpeed, double zeroTo60, int HP, int CC, int cylinders, int RPM)
         {
             this.name = name;
+            this.ID = ID;
             this.maxSpeed = maxSpeed;
             this.zeroTo60 = zeroTo60;
             this.HP = HP;
             this.CC = CC;
             this.cylinders = cylinders;
             this.RPM = RPM;
+        }
+
+        public CarCard Compare(CarCard other, category c) 
+        {
+            if (other == null) return this;
+
+            if (c == category.maxSpeed) 
+                if (maxSpeed > other.maxSpeed)
+                    return this;
+                else
+                    return other;
+
+            if (c == category.zeroTo60)
+                if (zeroTo60 < other.zeroTo60)
+                    return this;
+                else
+                    return other;
+
+            if (c == category.HP)
+                if (HP > other.HP)
+                    return this;
+                else
+                    return other;
+
+            if (c == category.CC)
+                if (CC > other.CC)
+                    return this;
+                else
+                    return other;
+
+            if (c == category.cylinders)
+                if (cylinders > other.cylinders)
+                    return this;
+                else
+                    return other;
+
+            if (c == category.RPM)
+                if (RPM > other.RPM)
+                    return this;
+                else
+                    return other;
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return name + ":\n\tname:" + name
+                + "\n\tID:" + ID
+                + "\n\tmaxspeed:" + maxSpeed
+                + "\n\tzeroTo60:" + zeroTo60
+                + "\n\tHP:" + HP
+                + "\n\tCC:" + CC
+                + "\n\tcylinders:" + cylinders
+                + "\n\tRPM:" + RPM;
         }
     }
 }
--- Quartet/Quartet/Program.cs
+++ Quartet/Quartet/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Quartet
 {
@@ -10,12 +11,14 @@ namespace Quartet
     {
         static void Main(string[] args)
         {
-            //string name, int maxSpeed, double zeroTo60, int hP, int cC, int cylinders, int rPM
-            CarCard C4 = new CarCard("Corvette Coupe", 281, 5.2, 344, 5400, 8, 5665);
-            CarCard C3 = new CarCard("Porshe 911 Targa", 285, 5.2, 320, 3596, 6, 6800);
-            CarCard E3 = new CarCard("BMW 645 Ci", 250, 5.6, 333, 4938, 8, 6100);
-            CarCard F4 = new CarCard("Fisker Tramanto", 325, 3.6, 610, 5439, 8, 6100);
-            CarCard H4 = new CarCard("BMW M5", 250, 4.7, 507, 4999, 10, 7750);
+            //string name, string ID, int maxSpeed, double zeroTo60, int HP, int CC, int cylinders, int RPM
+            CarCard C4 = new CarCard("Corvette Coupe", "C4", 281, 5.2, 344, 5400, 8, 5665);
+            CarCard C3 = new CarCard("Porshe 911 Targa", "C3", 285, 5.2, 320, 3596, 6, 6800);
+            CarCard E3 = new CarCard("BMW 645 Ci", "E3", 250, 5.6, 333, 4938, 8, 6100);
+            CarCard F4 = new CarCard("Fisker Tramanto", "F4", 325, 3.6, 610, 5439, 8, 6100);
+            CarCard H4 = new CarCard("BMW M5", "H4", 250, 4.7, 507, 4999, 10, 7750);
+            WriteLine(C4.Compare(E3, CarCard.category.cylinders));
+            ReadLine();
         }
     }
 }

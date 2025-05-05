d64b16622057ed9004565f6ddcb0327732ce5f43
mjors485 <mjors485@learner.dvusd.org>
2025-05-05 09:31:08 -0700

    5/5 scoring start

---
+++ Scoring/Scoring/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Scoring
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Score1TB = new System.Windows.Forms.TextBox();
+            this.Score2TB = new System.Windows.Forms.TextBox();
+            this.Score3TB = new System.Windows.Forms.TextBox();
+            this.Score4TB = new System.Windows.Forms.TextBox();
+            this.Score5TB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Boulder Creek";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Finisher";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Finisher";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Score:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "O\'Connor";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(307, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Finisher";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(307, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Score:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(307, 45);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Deer Valley";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(442, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Finisher";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(442, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Score:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(442, 45);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "Goldwater";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(577, 74);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Finisher";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(577, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Score:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(577, 45);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "Ridge";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(29, 121);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(315, 212);
+            this.listBox1.TabIndex = 15;
+            // 
+            // Score1TB
+            // 
+            this.Score1TB.Location = new System.Drawing.Point(87, 23);
+            this.Score1TB.Name = "Score1TB";
+            this.Score1TB.ReadOnly = true;
+            this.Score1TB.Size = new System.Drawing.Size(42, 22);
+            this.Score1TB.TabIndex = 16;
+            this.Score1TB.Text = "000";
+            // 
+            // Score2TB
+            // 
+            this.Score2TB.Location = new System.Drawing.Point(225, 23);
+            this.Score2TB.Name = "Score2TB";
+            this.Score2TB.ReadOnly = true;
+            this.Score2TB.Size = new System.Drawing.Size(42, 22);
+            this.Score2TB.TabIndex = 17;
+            this.Score2TB.Text = "000";
+            // 
+            // Score3TB
+            // 
+            this.Score3TB.Location = new System.Drawing.Point(365, 23);
+            this.Score3TB.Name = "Score3TB";
+            this.Score3TB.ReadOnly = true;
+            this.Score3TB.Size = new System.Drawing.Size(42, 22);
+            this.Score3TB.TabIndex = 18;
+            this.Score3TB.Text = "000";
+            // 
+            // Score4TB
+            // 
+            this.Score4TB.Location = new System.Drawing.Point(500, 23);
+            this.Score4TB.Name = "Score4TB";
+            this.Score4TB.ReadOnly = true;
+            this.Score4TB.Size = new System.Drawing.Size(42, 22);
+            this.Score4TB.TabIndex = 19;
+            this.Score4TB.Text = "000";
+            // 
+            // Score5TB
+            // 
+            this.Score5TB.Location = new System.Drawing.Point(635, 23);
+            this.Score5TB.Name = "Score5TB";
+            this.Score5TB.ReadOnly = true;
+            this.Score5TB.Size = new System.Drawing.Size(42, 22);
+            this.Score5TB.TabIndex = 20;
+            this.Score5TB.Text = "000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Score5TB);
+            this.Controls.Add(this.Score4TB);
+            this.Controls.Add(this.Score3TB);
+            this.Controls.Add(this.Score2TB);
+            this.Controls.Add(this.Score1TB);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox Score1TB;
+        private System.Windows.Forms.TextBox Score2TB;
+        private System.Windows.Forms.TextBox Score3TB;
+        private System.Windows.Forms.TextBox Score4TB;
+        private System.Windows.Forms.TextBox Score5TB;
+    }
+}
+
---
+++ Scoring/Scoring/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scoring
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            ScoreRace();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox2.Text);
+            ScoreRace();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox3.Text);
+            ScoreRace();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox4.Text);
+            ScoreRace();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox5.Text);
+            ScoreRace();
+        }
+
+        private void ScoreRace()
+        {
+            int f1 = 0, f2 = 0, f3 = 0, f4 = 0, f5 = 0;
+            int[] t1 = new int[7];
+            int[] t2 = new int[7];
+            int[] t3 = new int[7];
+            int[] t4 = new int[7];
+            int[] t5 = new int[7];
+            int score = 0;
+            foreach (var item in listBox1.Items)
+            {
+                score++;
+                if (item.ToString() == textBox1.Text)
+                    t1[f1++] = score;
+                if (item.ToString() == textBox2.Text)
+                    t2[f2++] = score;
+                if (item.ToString() == textBox3.Text)
+                    t3[f3++] = score;
+                if (item.ToString() == textBox4.Text)
+                    t4[f4++] = score;
+                if (item.ToString() == textBox5.Text)
+                    t5[f5++] = score;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                int temp = 0;
+                temp += t1[i];
+                Score1TB.Text = temp.ToString();
+            }
+        }
+    }
+}
